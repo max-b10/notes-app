dc3adcd43235114057421729c75d997375aaf425
max-b10 <bungaymax@gmail.com>
2021-02-08 12:59:42 +0000

    Refactored with the new functions.js file

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,37 +1,8 @@
-const notes = [
-  {
-    title: "My next trip",
-    body: "I would like to go to Spain",
-  },
-  {
-    title: "Habits to work on",
-    body: "Exercise. Eat a bit better.",
-  },
-  {
-    title: "Office modification",
-    body: "Get a new seat",
-  },
-];
+const notes = getSavedNotes();
 // Creating a filter system. When user searches in the input field, the filtered notes will
 // be appended to the empty div.
 const filters = {
   searchText: "",
-};
-// A function to show the notes which match the search text
-// i.e. the filtered notes. So the title text matches the filter/search text.
-const renderNotes = function (notes, filters) {
-  const filteredNotes = notes.filter(function (note) {
-    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
-  });
-  // Using innerHTML wipes out the div of any previous html before adding the filtered notes.
-  // Whatever is in the quotes would be rendered to the div.
-  document.querySelector("#notes").innerHTML = "";
-  // The filtered note is appended to the empty div (#notes).
-  filteredNotes.forEach(function (note) {
-    const noteEl = document.createElement("p");
-    noteEl.textContent = note.title;
-    document.querySelector("#notes").appendChild(noteEl);
-  });
 };
 
 // Calling renderNotes will present the latest notes and filters.
@@ -39,9 +10,14 @@ renderNotes(notes, filters);
 
 // Add event listener click. Changes text content of the event target i.e. the button.
 document.querySelector("#create-note").addEventListener("click", function (e) {
-  e.target.textContent = "Clicked";
-  // console.log("clicked");
-  // console.log(e);
+  // e.target.textContent = "Clicked";
+  //push an object onto notes array and save what's added to local storage.
+  notes.push({
+    title: "",
+    body: "",
+  });
+  saveNotes(notes);
+  renderNotes(notes, filters);
 });
 
 // The empty 'filters' object above is being filled with the user input value.
@@ -53,3 +29,23 @@ document.querySelector("#search-text").addEventListener("input", function (e) {
 document.querySelector("#filter-by").addEventListener("change", function (e) {
   console.log(e.target.value);
 });
+
+//CRUD local storage operations:
+// localStorage.setItem("location", "Cambridge");
+// console.log(localStorage.getItem("location"));
+// localStorage.removeItem("location");
+// localStorage.clear();
+// const user = {
+//   name: "Bob",
+//   age: 50,
+// };
+// // JSON. stringify takes in your array or object etc and returns it as a string.
+// const userJSON = JSON.stringify(user);
+// console.log(userJSON);
+// localStorage.setItem("user", userJSON);
+// converts the JSON into an Object. So you an use .notation to access the data
+// (in a template string for example).
+// const userJson = localStorage.getItem("user");
+// const user = JSON.parse(userJson);
+// console.log(`${user.name} is ${user.age}`);
+// console.log(userJson);
